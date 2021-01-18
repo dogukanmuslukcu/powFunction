e2a252cc4ee983b100741acfb6f5bb04617d799e
Dogukan Muslukcu <202003011008@dogus.edu.tr>
2021-01-18 21:26:52 +0300

    proje yorum satırları ingilizce olarak eklendi.

--- powFunction/Source.cpp
+++ powFunction/Source.cpp
@@ -9,34 +9,34 @@ int main() {
 
 	double eps = 0.001;
 	double xKeyfi;
-	double sonuc;
+	int sonuc;
 	double usAlinacakSayi;
 	double us;
 
 	cout << "usalinacak sayiyi giriniz : " << endl;
 	cin >> usAlinacakSayi;
-	cout << "sayinn ussunu bolume gore ters halini giriniz : " << endl;
+	cout << "sayinn ussunu , bolume gore ters halini giriniz : " << endl;
 	cin >> us;
 	cout << "xkeyfi giriniz : ! xkeyfi degeriniz usalinacak sayinizdan buyuk olmalidir!" << endl;
 	cin >> xKeyfi;
 
 
-	while (fabs(fx(xKeyfi, us, usAlinacakSayi)) > eps) { //Newton Raphson yöntemi ile ile kök bulma.
+	while (fabs(fx(xKeyfi, us, usAlinacakSayi)) > eps) { //Newton Raphson yöntemi ile ile kök bulma. [  find a root with newton raphson methot  ]
 		sonuc = xKeyfi - fx(xKeyfi, us, usAlinacakSayi) / fxtrv(xKeyfi, us);
 		xKeyfi = sonuc;
 	}
 	cout << "sonucunuz :" << endl;
 	cout << sonuc;
 }
-double fx(double xKeyfi, double us, double usAlinacakSayi) { // f(x) fonksiyonu
+double fx(double xKeyfi, double us, double usAlinacakSayi) { // f(x) fonksiyonu . [ f(x) function ]
 	double usfonk(double x, double y);
 	return (usfonk(xKeyfi, us) - usAlinacakSayi);
 }
-double fxtrv(double xKeyfi, double us) {  // f(x) fonksiyonun türevi.
+double fxtrv(double xKeyfi, double us) {  // f(x) fonksiyonun türevi.[  f(x)'s differential function  ]
 	double usfonk(double x, double y);
 	return (us * usfonk(xKeyfi, us - 1));
 }
-double usfonk(double x, double y) { // üssü pozitif lan sayýlarda recursýve ile pow fonksiyonu .
+double usfonk(double x, double y) { // üssü pozitif lan sayilarda recursive ile pow fonksiyonu .[ this function is recursive function when the power of the base > 0( pozitive ) ]
 	if (y == 1) {
 		return x;
 	}
